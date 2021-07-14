67628646a9f2789c604a82ad5d9b8f419e154c0b
Jessica Maldonado <jessica.maldonado@sas.com>
2021-07-13 21:08:04 -0400

    added Student header and implementation except for display

---
+++ Student.h
@@ -0,0 +1,32 @@
+#include <string>
+
+using namespace std;
+
+class Student
+{
+    public:
+        Student();
+        Student(string, string, string, double[4]);
+        ~Student();
+        string getFirstName();
+        void setFirstName(string);
+        string getLastName();
+        void setLastName(string);
+        string getSSN();
+        void setSSN(string);
+        double getAvgScores();
+        void setScores(double[4]);
+        double* getScores();
+        void displayStudents();
+        static int numStudents;
+        static int getNumStudents();
+
+    private:
+        string firstName = "";
+        string lastName = "";
+        string ssn = "";
+        double scores[4] = {0};
+        double calcAverage(double[]);
+    
+    
+};
